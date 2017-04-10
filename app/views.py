011050531a62432c30194a1fde16ed55af34c001
melur123 <meluridyllic@gmail.com>
2017-04-11 04:37:22 +0530

    fdf
    dgf

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render , HttpResponse
+import requests
+import nltk
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'app/base.html')
+
+def test(request):
+#from nltk import FreqDist
+#from nltk.book import *
+	readFile = open("C:\\Users\\lucifer\\pythonWebApp\\app\\test.txt","r")
+	#outputFile = open("output.txt","w+")
+	conatant = readFile.read()
+	text = nltk.word_tokenize(conatant)
+	text1 = nltk.Text(text)
+	a  = nltk.pos_tag(text)
+	#return HttpResponse(a)
+	cbc ="abc"
+	return render(request, 'app/words.html',{ 'words' : cbc})
+    
+def base(request):
+	return HttpResponse(" hsgdhsdg ")
